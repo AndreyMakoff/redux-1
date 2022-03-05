faa07c2b22aeea6beb5d81f1da43517bd24ad043
AndreyMakoff <95307424+AndreyMakoff@users.noreply.github.com>
2022-03-05 21:07:26 +0400

    redux

---
+++ src/store/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { logger } from './middleware/logger'
+import taskReducer from './task'
+import { combineReducers } from 'redux'
+import errorReducer from './errors'
+
+const rootReducer = combineReducers({
+  errors: errorReducer,
+  tasks: taskReducer,
+})
+
+function createStore() {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    devTools: process.env.NODE_ENV !== 'production',
+  })
+}
+
+export default createStore
